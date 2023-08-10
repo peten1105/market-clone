04b6b22fff45b45b75e53b5962d886e0853f4ad8
peten1105 <peten@kakao.com>
2023-08-11 02:12:38 +0900

    svelte, vite로 바꾼 후 firebase deploy 세팅

---
+++ src/pages/Login.svelte
@@ -0,0 +1,16 @@
+<div>Login 페이지입니다.</div>
+
+<form id="login-form" action="/signup" method="POST">
+    <div>
+      <label for="id">아이디</label>
+      <input type="text" id="id" name="id" required />
+    </div>
+    <div>
+      <label for="password">패스워드</label>
+      <input type="password" id="password" name="password" required />
+    </div>
+    <div>
+      <button type="submit">로그인</button>
+    </div>
+    <div id="info"></div>
+  </form>
---
+++ src/pages/Signup.svelte
@@ -0,0 +1,28 @@
+<div>Signup 페이지입니다.</div>
+
+<form id="signup-form" action="/signup" method="POST">
+    <div>
+      <label for="id">아이디</label>
+      <input type="text" id="id" name="id" required />
+    </div>
+    <div>
+      <label for="password">패스워드</label>
+      <input type="password" id="password" name="password" required />
+    </div>
+    <div>
+      <label for="password2">패스워드확인</label>
+      <input type="password" id="password2" name="password2" required />
+    </div>
+    <div>
+      <label for="name">이름</label>
+      <input type="text" id="name" name="name" required />
+    </div>
+    <div>
+      <label for="email">이메일</label>
+      <input type="text" id="email" name="email" />
+    </div>
+    <div>
+      <button type="submit">회원 가입하기</button>
+    </div>
+    <div id="info"></div>
+  </form>
